722da981f47968694cd09bcb4a7871c27b8d4ce7
alejandrof <aforero979@gmail.com>
2026-04-27 21:55:25 -0500

    suma de precios del fake API

--- EnClase/27-04-fake-api/index.js
+++ EnClase/27-04-fake-api/index.js
@@ -3,14 +3,30 @@ const API = "https://fakeapi.net/products"
 async function getProducts() {
 
     try {
-         const response = await fetch(API);
+
+        const response = await fetch(API);
         const data = await response.json();
 
-        console.log(data);
+        console.log(data.data);
+
+        let total = 0;
+        for (let i = 0; i < data.data.length; i++) {
+            total = total + data.data[i].price
+   
+        }
+
+        console.log(total.toFixed(2))
+
     } catch (error) {
-        console.error("Error al conectarse al FakeAPI")       
+
+        console.error("Error al conectarse al FakeAPI")
+
     }
+
+
 
 };
 
 getProducts()
+
+
